c4ad0f14a48bd4229a07f9ac16cedf623a62d6bd
arvinxx <arvinx@foxmail.com>
2020-07-17 19:46:56 +0800

    doc: 增加生成 symbol 的能力

--- example/pages/index.tsx
+++ example/pages/index.tsx
@@ -2,12 +2,19 @@ import React, { useState } from 'react';
 import { Button, Row, Col } from 'antd';
 import ReactJson from 'react-json-view';
 
-import { nodeToSketchGroup } from '../../lib';
+import { nodeToSketchGroup, parserSymbol } from '../../lib';
 export default () => {
   const [json, setJSON] = useState({});
   const generate = () => {
     const el = document.getElementById('test');
     const json = nodeToSketchGroup(el).toSketchJSON();
+
+    setJSON(json);
+  };
+  const generateSymbol = () => {
+    const el = document.getElementById('test');
+    const json = parserSymbol(el).toSketchJSON();
+
     setJSON(json);
   };
 
@@ -15,10 +22,10 @@ export default () => {
     <Row>
       <Col span={12}>
         <div id="test">
-          {/*<Button type={'dashed'}>测试</Button>*/}
-          <span>测试</span>
+          <Button type={'dashed'}>测试</Button>
         </div>
         <Button onClick={generate}>转换为 Group</Button>
+        <Button onClick={generateSymbol}>转换为 Symbol</Button>
       </Col>
       <Col span={12}>
         <ReactJson src={json} />
