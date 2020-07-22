b0bcd4e4ddc4adaef7e6a6bab43b6034ca895035
arvinxx <arvinx@foxmail.com>
2020-07-22 20:48:18 +0800

    test: Add test

---
+++ src/parser/__test__/colorAsset.test.ts
@@ -0,0 +1,29 @@
+import colorAsset from '../colorAsset';
+
+describe('colorAsset', () => {
+  beforeAll(() => {
+    document.body.innerHTML = `  
+    <table>
+      <tbody>
+        <thead>
+          <tr>
+            <th class="table" >
+              Header 1
+              <span/>
+            </th>
+          </tr>
+        </thead>
+      </tbody>
+    </table>
+    <div id="text">文本对象</div>
+    <div id="no-text"></div>
+    `;
+  });
+  afterAll(() => {
+    document.body.innerHTML = '';
+  });
+
+  it('RGB should work fine', function () {
+    // const color = colorAsset()
+  });
+});
