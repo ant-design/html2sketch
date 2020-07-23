1f865202f1f9a1339350aad53dc580a52d7ebe8a
arvinxx@iMac <arvinx@foxmail.com>
2020-07-24 00:23:33 +0800

    wip: 一个错误版本的 convertPathToShapeGroup 用于记录

--- src/model/Layer/Svg.ts
+++ src/model/Layer/Svg.ts
@@ -1,4 +1,4 @@
-import { SVGPathData, SVGPathDataParser } from 'svg-pathdata';
+import { SVGPathData } from 'svg-pathdata';
 import {
   CommandA,
   CommandC,
@@ -7,9 +7,11 @@ import {
   CommandS,
   SVGCommand,
 } from 'svg-pathdata/lib/types';
-import Base, { BaseLayerParams } from './Base';
+import { BaseLayerParams } from './Base';
 import { SketchFormat } from '../../index';
-import { defaultExportOptions } from '../utils';
+import ShapeGroup from './ShapeGroup';
+import ShapePath from './ShapePath';
+import Frame from '../Frame';
 
 export type SVG = {
   _class: 'svg';
@@ -19,7 +21,13 @@ export type SVG = {
   hasClippingMask: boolean;
 };
 
-type SvgPoint = CommandM | CommandL | CommandC | CommandS | CommandA;
+export type BezierPoint = CommandM | CommandL | CommandC | CommandS | CommandA;
+
+interface SvgShape {
+  points: BezierPoint[];
+  frame: Frame;
+  isClose: boolean;
+}
 
 interface SvgInitParams extends BaseLayerParams {
   path: string;
@@ -28,22 +36,25 @@ interface SvgInitParams extends BaseLayerParams {
 /**
  * SVG 对象
  */
-class Svg extends Base {
+class Svg extends ShapeGroup {
   rawSVGString: string;
   constructor({ x, y, width, height, path }: SvgInitParams) {
     super({ height, width, y, x });
     this.class = 'svg';
-    this.name = '路径';
+
+    this.name = 'svg';
+
     this.rawSVGString = path;
-    const { isClose, points } = Svg.convertToCubicBezier(path);
-    this.isClosed = isClose;
-    // @ts-ignore
-    this.points = points;
+    const { shapes, frame: groupFrame } = Svg.convertPathToShapeGroup(path);
+
+    this.layers = shapes.map((shape) => {
+      const { points, isClose, frame } = shape;
+      return new ShapePath({ points, isClose, ...frame.toJSON() });
+    });
+
+    this.frame.width = groupFrame.width;
+    this.frame.height = groupFrame.height;
   }
-  /**
-   * 是否是闭合路径
-   **/
-  isClosed?: boolean;
 
   /**
    * 转为 Sketch String
@@ -58,131 +69,58 @@ class Svg extends Base {
       hasClippingMask: this.hasClippingMask,
     };
   };
-  points: SvgPoint[];
-
-  /**
-   * 将内部点转为 Sketch Point
-   */
-  convertPointsToSketchPoint = (
-    point: SvgPoint,
-    index: number
-  ): SketchFormat.CurvePoint => {
-    let curveMode = SketchFormat.CurveMode.Straight;
-
-    let hasCurveFrom = false;
-    let hasCurveTo = false;
-
-    const prevIndex = index - 1 > -1 ? index - 1 : this.points.length - 1;
-    const nextIndex = index + 1 < this.points.length ? index + 1 : 0;
-    const prevPoint = this.points[prevIndex];
-    const nextPoint = this.points[nextIndex];
-
-    let thisPoint = point;
-    let curveFromPoint: { x: number; y: number } = thisPoint;
-    let curveToPoint: { x: number; y: number } = thisPoint;
-
-    if (point.type === SVGPathData.MOVE_TO && index === 0) {
-      thisPoint = this.points[this.points.length - 1];
-    }
-
-    // 解析曲线模式
-    if (
-      prevPoint.type !== thisPoint.type ||
-      nextPoint.type !== thisPoint.type
-    ) {
-      curveMode = SketchFormat.CurveMode.Disconnected;
-    }
-
-    // 如果自身点是 Curve 点
-    if (thisPoint.type === SVGPathData.CURVE_TO) {
-      curveToPoint = { x: thisPoint.x2, y: thisPoint.y2 };
-      hasCurveTo = true;
-    }
-
-    // 如果下一个点是 Curve 点
-    if (
-      point.type === SVGPathData.LINE_TO &&
-      nextPoint.type === SVGPathData.CURVE_TO
-    ) {
-      hasCurveFrom = true;
-      curveFromPoint = { x: nextPoint.x1, y: nextPoint.y1 };
-    }
-
-    if (index === this.points.length - 1)
-      // 返回最后一个对象
-      return;
-    return {
-      _class: 'curvePoint',
-      cornerRadius: 0,
-      curveFrom: `{${curveFromPoint.x}, ${curveFromPoint.y}}`,
-      curveMode,
-      curveTo: `{${curveToPoint.x}, ${curveToPoint.y}}`,
-      hasCurveFrom,
-      hasCurveTo,
-      point: `{${point.x}, ${point.y}}`,
-    };
-  };
-
-  /**
-   * 转换为 Sketch ShapePath 对象
-   **/
-  toSketchJSON = (): SketchFormat.ShapePath => {
-    return {
-      _class: 'shapePath',
-      do_objectID: this.id,
-      booleanOperation: -1,
-      isFixedToViewport: false,
-      isFlippedHorizontal: false,
-      isFlippedVertical: false,
-      isLocked: false,
-      isVisible: true,
-      layerListExpandedType: 0,
-      name: this.name,
-      nameIsFixed: false,
-      resizingConstraint: 63,
-      resizingType: 0,
-      rotation: 0,
-      shouldBreakMaskChain: false,
-      exportOptions: defaultExportOptions,
-      frame: this.frame.toSketchJSON(),
-      clippingMaskMode: 0,
-      hasClippingMask: false,
-      style: this.style.toSketchJSON(),
-      edited: true,
-      isClosed: this.isClosed,
-      pointRadiusBehaviour: 1,
-      points: this.points.map(this.convertPointsToSketchPoint).filter((l) => l),
-    };
-  };
 
   /**
    * 将 Path 转为贝赛尔曲线
-   * @param path 路径
+   * @param svgPath 路径
    */
-  static convertToCubicBezier = (path: string) => {
-    const svgPathData = new SVGPathData(path);
-    const bounds = svgPathData.getBounds();
-    const frame = {
+  static convertPathToShapeGroup = (
+    svgPath: string
+  ): { shapes: SvgShape[]; frame: { width: number; height: number } } => {
+    // 获取 shapeGroup 的 frame
+    let shapeGroup = new SVGPathData(svgPath);
+    const bounds = shapeGroup.getBounds();
+    const groupFrame = {
       width: bounds.maxX - bounds.minX,
       height: bounds.maxY - bounds.minY,
     };
+
+    // 将 shape group 根据点类型进行划分
+    let shapes = [];
+
+    let points = [];
     const { minX, minY } = bounds;
 
-    // 判断是否闭合
-    const isClose = svgPathData.commands.findIndex((i) => i.type === 1) > -1;
-
-    const newPath = svgPathData
+    shapeGroup = shapeGroup
       .translate(-minX, -minY)
-      .aToC() // 将所有圆弧转为 curve
-      .normalizeHVZ() // 将 HVZ 转为直线
-      .normalizeST() // 将 smooth curve 转为curve
-      .transform(Svg.normalizationXY(frame.width, frame.height))
-      .toAbs()
-      .encode();
-    const points = new SVGPathDataParser().parse(newPath);
+      // .aToC() // 将所有圆弧转为 curve
+      // .normalizeHVZ() // 将 HVZ 转为直线
+      // .normalizeST() // 将 smooth curve 转为curve
+      .transform(Svg.normalizationXY(groupFrame.width, groupFrame.height))
+      .toAbs();
 
-    return {
-      points: points
+    // 拆分 shape
+    for (const command of shapeGroup.commands) {
+      // 碰到 Move_TO 且这个时候 shape 里面有东西
+      // 清空 shape 并把 shape 添加到 shape里
+      if (command.type === SVGPathData.MOVE_TO && points.length > 0) {
+        shapes.push({ points });
+        points = [];
+      }
+      // 正常情况下直接加入这个指令
+      points.push(command);
+    }
+    // 将最后一组shape 加入 shapes 中
+    if (points.length > 0) {
+      shapes.push({ points });
+    }
+
+    // 对 shapes 进行重构
+    shapes = shapes.map((shape) => {
+      // 判断shape是否闭合
+      const isClose = shape.points.findIndex((i) => i.type === 1) > -1;
+
+      const points = shape.points
         .filter(
           (i) =>
             // 清理 Z
@@ -193,9 +131,13 @@ class Svg extends Base {
           const { relative, ...res } = i;
 
           return res;
-        }),
-      frame,
-      isClose,
+        });
+      return { points, isClose };
+    });
+
+    return {
+      shapes,
+      frame: groupFrame,
     };
   };
 
