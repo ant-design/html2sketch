cf253c82877eaf8f565f026cdd425e2a59238479
arvinxx <arvinx@foxmail.com>
2020-08-26 22:29:43 +0800

    config: 调整网页配置项

--- config/config.ts
+++ config/config.ts
@@ -4,6 +4,9 @@ export default defineConfig({
   title: 'html2sketch',
   logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
   mode: 'site',
+  // 部署在非根目录时, base 和 publicPath 都需要配置
+  base: '/html2sketch/',
+  publicPath: '/html2sketch/',
   extraBabelPlugins: [
     [
       'import',
