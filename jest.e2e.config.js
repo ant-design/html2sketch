c3c3147a7932aa853e44e552f704286e75826f26
arvinxx@iMac <arvinx@foxmail.com>
2020-09-27 00:37:51 +0800

    :wrench: chore: update config

--- jest.e2e.config.js
+++ jest.e2e.config.js
@@ -7,7 +7,7 @@ module.exports = {
   moduleNameMapper: {
     uuid: '<rootDir>/test/__mocks__/uuid.ts',
     html2sketch: '<rootDir>/src',
-    '@e2e': '<rootDir>/e2e/__utils__',
+    '@e2e-utils': '<rootDir>/e2e/__utils__',
   },
   setupFilesAfterEnv: ['./e2e/setupE2E.ts'],
   testMatch: ['**/e2e/**/?(*.)+(e2e).ts'],
