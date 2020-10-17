9d2276639be927814c9d211e07be1d792ac42ea4
arvinxx <arvinx@foxmail.com>
2020-10-17 19:32:17 +0800

    📝 docs: update svg parsing demo

---
+++ docs/e2e/svg/demos/SvgBackground.tsx
@@ -0,0 +1,21 @@
+import React, { FC } from 'react';
+import { useElements, TestLayout } from '@docs-utils';
+// @ts-ignore
+import { ReactComponent as Svg } from './svg/background.svg';
+
+/**
+ * 插画解析 demos
+ */
+const SvgIllustration: FC = () => {
+  const { elements, ref } = useElements();
+
+  return (
+    <TestLayout elements={elements}>
+      <div ref={ref} style={{ background: '#1270cf' }}>
+        <Svg />
+      </div>
+    </TestLayout>
+  );
+};
+
+export default SvgIllustration;
