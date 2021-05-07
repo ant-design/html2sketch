573ec8ecc9b0e018dfb632163da752731b7fde0e
arvinxx <arvinx@foxmail.com>
2021-05-08 00:17:15 +0800

    :green_heart: test: fix e2e

--- e2e/__tests__/components/modal.e2e.ts
+++ e2e/__tests__/components/modal.e2e.ts
@@ -3,19 +3,18 @@ import { initHtml2Sketch, isUpdate, outputJSONData } from '@e2e-utils';
 
 describe('测试 Modal 类型', () => {
   test('Modal 正常', async () => {
-    const html2Sketch = await initHtml2Sketch({ debug: true });
+    const html2Sketch = await initHtml2Sketch();
     function selector(dom: Document) {
       const item = dom.getElementsByClassName('ant-modal')?.item(0);
       if (!item) throw Error('没找到 item');
       return item;
     }
 
-    await html2Sketch.page.waitFor(3000);
-
     const symbol = await html2Sketch.nodeToSymbol(
       '/components/modal',
       selector,
     );
+
     expect(symbol).toBeTruthy();
 
     if (isUpdate) {
