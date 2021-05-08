ca0b74b5309de8d0019ec38080081e015bb58702
Arvin Xu <arvinx@foxmail.com>
2021-05-08 14:05:18 +0800

    :green_heart: ci: fix release script

--- .releaserc.js
+++ .releaserc.js
@@ -29,7 +29,7 @@ module.exports = {
       {
         assets: ['CHANGELOG.md', 'package.json'],
         message:
-          ':bookmark: chore(release): v${nextRelease.version} [skip ci]\n\nhttps://github.com/ant-design/html2sketch/releases/tag/${nextRelease.gitTag}',
+          ':bookmark: chore(release): ${nextRelease.gitTag} [skip ci] \n\n${nextRelease.notes}',
       },
     ],
   ],
