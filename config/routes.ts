7cd463e3c5b99b57018c7fb99725260b01ce7b66
Arvin Xu <arvinx@foxmail.com>
2022-11-21 13:23:10 +0800

    :wrench: docs: 优化文档配置，解决二级目录 SideBar 不匹配的问题

--- config/routes.ts
+++ config/routes.ts
@@ -1,12 +1,113 @@
-export const nav = [
-  { title: '指南', link: '/guide' },
-  { title: 'sketch组件', link: '/sketch/button' },
-  { title: '解析用例', link: '/e2e' },
-  {
-    title: 'GitHub',
-    link: 'https://github.com/ant-design/html2sketch',
-  },
-];
+interface RouteItem {
+  title: string;
+  link: string;
+}
+
+interface SideBarRoute {
+  title?: string;
+  children: RouteItem[];
+}
+
+const basic: SideBarRoute = {
+  title: '基础用例解析',
+  children: [
+    {
+      title: '坐标与尺寸解析',
+      link: '/e2e/basic/position',
+    },
+    {
+      title: '伪类解析',
+      link: '/e2e/basic/pseudo',
+    },
+    {
+      title: '文本解析',
+      link: '/e2e/basic/text',
+    },
+    {
+      title: '背景图片解析',
+      link: '/e2e/basic/background',
+    },
+    {
+      title: '图片解析',
+      link: '/e2e/basic/image',
+    },
+    {
+      title: 'Canvas 解析',
+      link: '/e2e/basic/canvas',
+    },
+  ],
+};
+
+const svg: SideBarRoute = {
+  title: 'Svg 解析',
+  children: [
+    {
+      title: 'Svg 解析逻辑',
+      link: '/e2e/svg',
+    },
+    {
+      title: '基础解析',
+      link: 'e2e/svg/basic',
+    },
+    {
+      title: 'Icon 解析',
+      link: 'e2e/svg/icon',
+    },
+    {
+      title: 'Illustration 解析',
+      link: 'e2e/svg/illustration',
+    },
+  ],
+};
+
+const styles: SideBarRoute = {
+  title: '特殊样式',
+  children: [
+    {
+      title: '渐变解析',
+      link: 'e2e/styles/gradient',
+    },
+    {
+      title: 'Transform 解析',
+      link: 'e2e/styles/transform',
+    },
+  ],
+};
+
+const antdComponents: SideBarRoute = {
+  title: 'Ant Design 组件',
+  children: [
+    {
+      title: 'Tabs',
+      link: 'e2e/components/tabs',
+    },
+    {
+      title: 'ProCard',
+      link: 'e2e/components/procard',
+    },
+    {
+      title: 'Radio 单选器',
+      link: 'e2e/components/radio',
+    },
+    {
+      title: 'Table',
+      link: 'e2e/components/table',
+    },
+    {
+      title: 'Tooltip',
+      link: 'e2e/components/tooltip',
+    },
+    {
+      title: 'Modal 浮层',
+      link: 'e2e/components/modal',
+    },
+  ],
+};
+
+const goBack: SideBarRoute = {
+  children: [{ title: '🔙 返回上级', link: '/e2e' }],
+};
+
 export const sidebar = {
   '/guide': [
     {
@@ -55,99 +156,25 @@ export const sidebar = {
           title: '测试用例说明',
           link: '/e2e',
         },
-        {
-          title: ' Svg 解析逻辑',
-          link: '/e2e/svg',
-        },
       ],
     },
-    {
-      title: '基础用例解析',
-      children: [
-        {
-          title: '坐标与尺寸解析',
-          link: '/e2e/basic/position',
-        },
-        {
-          title: '伪类解析',
-          link: '/e2e/basic/pseudo',
-        },
-        {
-          title: '文本解析',
-          link: '/e2e/basic/text',
-        },
-        {
-          title: '背景图片解析',
-          link: '/e2e/basic/background',
-        },
-        {
-          title: '图片解析',
-          link: '/e2e/basic/image',
-        },
-        {
-          title: 'Canvas 解析',
-          link: '/e2e/basic/canvas',
-        },
-      ],
-    },
-    {
-      title: '特殊样式',
-      children: [
-        {
-          title: '渐变解析',
-          link: 'e2e/styles/gradient',
-        },
-        {
-          title: 'Transform 解析',
-          link: 'e2e/styles/transform',
-        },
-      ],
-    },
-    {
-      title: 'Svg 解析',
-      children: [
-        {
-          title: '基础解析',
-          link: 'e2e/svg/basic',
-        },
-        {
-          title: 'Icon 解析',
-          link: 'e2e/svg/icon',
-        },
-        {
-          title: 'Illustration 解析',
-          link: 'e2e/svg/illustration',
-        },
-      ],
-    },
-    {
-      title: 'Ant Design 组件',
-      children: [
-        {
-          title: 'Tabs',
-          link: 'e2e/components/tabs',
-        },
-        {
-          title: 'ProCard',
-          link: 'e2e/components/procard',
-        },
-        {
-          title: 'Radio 单选器',
-          link: 'e2e/components/radio',
-        },
-        {
-          title: 'Table',
-          link: 'e2e/components/table',
-        },
-        {
-          title: 'Tooltip',
-          link: 'e2e/components/tooltip',
-        },
-        {
-          title: 'Modal 浮层',
-          link: 'e2e/components/modal',
-        },
-      ],
-    },
+    basic,
+    styles,
+    svg,
+    antdComponents,
   ],
+  '/e2e/basic': [goBack, basic],
+  '/e2e/styles': [goBack, styles],
+  '/e2e/svg': [goBack, svg],
+  '/e2e/components': [goBack, antdComponents],
 };
+
+export const nav: RouteItem[] = [
+  { title: '指南', link: '/guide' },
+  { title: 'sketch组件', link: '/sketch/button' },
+  { title: '解析用例', link: '/e2e' },
+  {
+    title: 'GitHub',
+    link: 'https://github.com/ant-design/html2sketch',
+  },
+];
