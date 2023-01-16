5367a0b272394d926046a1880e36ae59e21bbbc1
MadCcc <1075746765@qq.com>
2023-01-16 12:03:42 +0800

    ✨ feat: support radius

--- src/utils/overflow.ts
+++ src/utils/overflow.ts
@@ -7,12 +7,16 @@ export const createOverflowMask = (node: Element, options?: { isInput?: boolean 
     height,
     x: 0,
     y: 0,
-    name: '蒙层',
   });
+  rect.name = 'Overflow 蒙层';
+
+  const nodeStyles = getComputedStyle(node);
+  if (nodeStyles.borderRadius) {
+    rect.cornerRadius = parseInt(nodeStyles.borderRadius, 10);
+  }
 
   if (options?.isInput) {
     // 输入框需要计算 padding
-    const nodeStyles = getComputedStyle(node);
     const paddingLeft = parseFloat(nodeStyles.getPropertyValue('padding-left'));
     const paddingTop = parseFloat(nodeStyles.getPropertyValue('padding-top'));
     const paddingRight = parseFloat(nodeStyles.getPropertyValue('padding-right'));
